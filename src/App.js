ad21f5dd2f2f53ed21fb80de19ae2e1dc515980c
ayah <yahoraual92@gmail.com>
2022-05-03 17:24:13 +0300

    Implemented removal of todos using Context

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import TodoList from "./Todo/TodoList";
-
+import Context from "./context";
 
 function App() {
   const [todos, setTodos] = React.useState([
@@ -20,13 +20,25 @@ function App() {
     setTodos(newTodos);
   }
 
+  function removeTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id));
+  }
 
   return (
-    <div className="wrapper">
+    <Context.Provider value={{removeTodo: removeTodo}}>
+      <div className="wrapper">
       <h1>React</h1>
-
-      <TodoList todos={todos} onToggle={toggleTodo} />
-    </div>
+      
+      {todos.length ? (
+        <TodoList todos={todos} onToggle={toggleTodo} />
+        ) : 
+        (
+          <p>No Todos</p>
+        )
+      }
+      
+      </div>
+    </Context.Provider>
   );
 };
 
