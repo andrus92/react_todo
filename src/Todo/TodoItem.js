90fd833b078ba943d4d8a5275a2e2adcb461f43b
ayah <yahoraual92@gmail.com>
2022-05-03 16:00:28 +0300

    Added styles for TodoItem

--- src/Todo/TodoItem.js
+++ src/Todo/TodoItem.js
@@ -1,10 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+const styles = {
+    li: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '.5rem 1rem',
+        border: '1px solid #ccc',
+        borderRadius: '4px',
+        marginBottom: '.5rem',
+    },
+    input: {
+        marginRight: '1rem',
+    }
+};
+
 function TodoItem(props) {
     return (
-        <li>
-            <strong>{props.index + 1} </strong>{props.todo.title}
+        <li style={styles.li}>
+            <span>
+                <input type="checkbox" style={styles.input}/>
+                <strong>{props.index + 1} </strong>{props.todo.title}
+            </span>
+
+            <button className="rm">&times;</button>
         </li>
     );
 };
